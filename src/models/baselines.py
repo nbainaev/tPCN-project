12471fdf9a752c715e831d1eb1e4bc812907b2b2
nbainaev <nik.baynaev@yandex.ru>
2025-07-04 01:09:18 +0300

    Updated baselines

--- src/models/baselines.py
+++ src/models/baselines.py
@@ -8,6 +8,7 @@ class FOPModel:
             self.mdp_mat = np.arange(self.n_obs).reshape((options['obs_size'] // 2, options['obs_size'] // 2))
         else:
             self.n_obs = options['obs_size']
+            self.obs_dict = {0: -1, 1: -1, 1: 0, 2: 1, 4: 2, 5: 3}
         self.dir = options['dir_size']
         self.count_matrix = np.zeros((self.n_obs, self.dir, self.n_obs))
         self.prev_obs = None
@@ -38,7 +39,7 @@ class FOPModel:
         if self.mode == 'pomdp':
             for i in range(obs.shape[0]):
                 pred_ = self.count_matrix[self.prev_obs[i], dir[i]].argmax()
-                preds.append(pred_ if pred_ < self.n_obs-2 else pred_ - self.n_obs)
+                preds.append(self.obs_dict[pred_])
         else:
             for i in range(obs.shape[0]):
                 enc_pred_obs = self.mdp_mat[self.prev_obs[i, 0], self.prev_obs[i, 1]]
@@ -90,11 +91,14 @@ class RandomModel:
             self.n_obs = options['obs_size'] // 2
         else:
             self.n_obs = options['obs_size']
-        
+        self.obs_dict = {0: -1, 1: -1, 1: 0, 2: 1, 4: 2, 5: 3}
+        self.min = min(list(self.obs_dict.values))
+        self.max = max(list(self.obs_dict.values))
         self._rng = np.random.default_rng(seed=42) 
     
     def observe_sequence(self, obss, dirs, init_pos):
         pass
     
     def predict_sequence(self, dirs, init_pos):
-        return self._rng.integers(self.n_obs, size=(dirs.shape[0], dirs.shape[1], 2 if self.mode == 'mdp' else 1))
+
+        return self._rng.integers(self.min, self.max, size=(dirs.shape[0], dirs.shape[1], 2 if self.mode == 'mdp' else 1))
